a2de3f18ea4f9bd491993740e0ba4a1ad043f6e7
Patryk Balecki <patrykbalecki@MACBOOKPRO-3F9F.station>
2025-02-09 00:39:52 +0000

    small change

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +11,9 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Add a redirect or response for the root URL
+urlpatterns += [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+]
  
